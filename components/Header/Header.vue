a478b69c8147ef0454c5246d26993a446c80c801
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-16 00:13:14 +0200

    remove same file and finish header component

---
+++ components/Header/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-between items-center px-14 py-4 border-b border-gray-300">
+        <HeaderLogo/>
+        <HeaderNav />
+    </div>
+</template>
---
+++ components/Header/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <img 
+            src="../../assets/img/logo.png" 
+            class="w-[120px]"
+            alt="logo airbnb"
+        >
+    </div>
+</template>
